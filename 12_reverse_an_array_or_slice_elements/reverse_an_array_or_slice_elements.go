876853c043f73b8b42668425bbd6a731454db8b3
avinash upadhyayula <avinash.upadhyayula@gmail.com>
2025-07-25 22:13:04 +0530

    continuation

--- 12_reverse_an_array_or_slice_elements/reverse_an_array_or_slice_elements.go
+++ 12_reverse_an_array_or_slice_elements/reverse_an_array_or_slice_elements.go
@@ -10,6 +10,13 @@ Reverse an array or slice elements
 
 package main
 
+import "fmt"
+
 func main() {
-	
+	arr := []int{3, 4, 2, 7, 1, 5}
+	fmt.Println("Value of arr:", arr)
+	for i, j := 0, len(arr)-1; i < j; i, j = i+1, j-1 {
+		arr[i], arr[j] = arr[j], arr[i]
+	}
+	fmt.Println("Reverse of arr:", arr)
 }
