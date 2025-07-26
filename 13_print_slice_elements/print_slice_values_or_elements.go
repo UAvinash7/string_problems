28c968ce11a7fde43d244b3b7a3ddb09100edba0
avinash upadhyayula <avinash.upadhyayula@gmail.com>
2025-07-26 21:55:38 +0530

    continuation

--- 13_print_slice_elements/print_slice_values_or_elements.go
+++ 13_print_slice_elements/print_slice_values_or_elements.go
@@ -8,10 +8,16 @@ Print a slice (array) values or elements
 
 // Solution
 
-
 package main
 
+import "fmt"
 
 func main() {
-	
+	arr := []int{1, 3, 5, 4, 2}
+	fmt.Println("arr:", arr)
+	for _, ele := range arr {
+		if ele == 2 {
+			fmt.Println("My ID is:", ele)
+		}
+	}
 }
