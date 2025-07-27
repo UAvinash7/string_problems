5f724b8b59bc24400885b7cbec4a1a936095e5c3
avinash upadhyayula <avinash.upadhyayula@gmail.com>
2025-07-27 22:39:48 +0530

    continuation

--- 15_convert_an_array_into_a_slice/convert_any_array_into_a_slice.go
+++ 15_convert_an_array_into_a_slice/convert_any_array_into_a_slice.go
@@ -10,6 +10,14 @@ Convert an array into a slice
 
 package main
 
+import "fmt"
+
 func main() {
-	
+	arr1 := [4]int{1, 2, 3, 4}
+	arr2 := []int{}
+	fmt.Println("Value of slice variable arrr2 is:", arr2)
+	arr2 = arr1[:]
+	fmt.Println("After assigning, the value of arr2 is:", arr2)
+	arr2 = append(arr2, 5)
+	fmt.Println("After appending, the value of arr2 is:", arr2)
 }
