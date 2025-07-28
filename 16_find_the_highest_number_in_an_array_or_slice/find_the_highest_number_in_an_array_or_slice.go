14a8abc70e55ec65c5c4d7e71944d990f89437c4
avinash upadhyayula <avinash.upadhyayula@gmail.com>
2025-07-28 22:09:48 +0530

    continuation

--- 16_find_the_highest_number_in_an_array_or_slice/find_the_highest_number_in_an_array_or_slice.go
+++ 16_find_the_highest_number_in_an_array_or_slice/find_the_highest_number_in_an_array_or_slice.go
@@ -10,6 +10,15 @@ Find the highest number in an array or slice
 
 package main
 
-func main() {
+import "fmt"
 
+func main() {
+	slice := []int{1, 4, 2, 6, 5, 3}
+	max_value := slice[0]
+	for i := 0; i < len(slice); i++ {
+		if slice[i] > max_value {
+			max_value = slice[i]
+		}
+	}
+	fmt.Println("maximum value of element in the slice:", max_value)
 }
