fc5e7734b6b834e7be8cebdbb67a6650179ad6c5
avinash upadhyayula <avinash.upadhyayula@gmail.com>
2025-07-29 22:32:07 +0530

    continuation

--- 17_find_the_smallest_number_in_an_array_or_slice/find_the_smallest_number_in_an_array_or_slice.go
+++ 17_find_the_smallest_number_in_an_array_or_slice/find_the_smallest_number_in_an_array_or_slice.go
@@ -10,6 +10,15 @@ Find the smallest number in an array or slice.
 
 package main
 
+import "fmt"
+
 func main() {
-	
+	slice := []int{2, 4, 6, 1, 7}
+	min_value := slice[0]
+	for i := 0; i < len(slice); i++ {
+		if min_value > slice[i] {
+			min_value = slice[i]
+		}
+	}
+	fmt.Println("Value of smallest Element:", min_value)
 }
