fab7c064ebfe34483f0389b7fb33220526aa45bb
avinash upadhyayula <avinash.upadhyayula@gmail.com>
2025-07-30 22:21:47 +0530

    continuation

--- 14_replace_elements_in_a_slice_or_array/replace_elements_in_a_slice_or_array.go
+++ 14_replace_elements_in_a_slice_or_array/replace_elements_in_a_slice_or_array.go
@@ -10,6 +10,13 @@ Replace elements in a slice or array
 
 package main
 
+import "fmt"
+
 func main() {
-	
+	arr1 := []int{1, 2, 3, 4}
+	arr2 := []int{6, 7, 8}
+	fmt.Println("arr1:", arr1)
+	copy(arr1[3:], arr2)
+	fmt.Println("new array:", arr1)
+	fmt.Println("second array:", arr2)
 }
