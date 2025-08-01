fe294d42c042e39559beff8ba33354cfde17c219
avinash upadhyayula <avinash.upadhyayula@gmail.com>
2025-08-01 22:24:02 +0530

    continuation

--- 23_alternatively_merge_two_strings/alternatively_merge_two_strings.go
+++ 23_alternatively_merge_two_strings/alternatively_merge_two_strings.go
@@ -10,6 +10,20 @@ Alternatively merge two strings.
 
 package main
 
+import "fmt"
+
 func main() {
-	
+	str1 := "Hello"
+	str2 := "Bye"
+	// Output =  HBeylelo
+	var result string
+	for i := 0; i < len(str1) || i < len(str2); i++ {
+		if i < len(str1) {
+			result += string(str1[i])
+		}
+		if i < len(str2) {
+			result += string(str2[i])
+		}
+	}
+	fmt.Println("Result:", result)
 }
