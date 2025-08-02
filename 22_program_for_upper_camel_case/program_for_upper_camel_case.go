c9ea685073053e83500c0c03869c864262b13a03
avinash upadhyayula <avinash.upadhyayula@gmail.com>
2025-08-02 22:41:34 +0530

    continuation

--- 22_program_for_upper_camel_case/program_for_upper_camel_case.go
+++ 22_program_for_upper_camel_case/program_for_upper_camel_case.go
@@ -10,6 +10,22 @@ Program for upper camel case
 
 package main
 
+import (
+	"fmt"
+	"strings"
+)
+
 func main() {
-	
+	str := "hello ram" // Hello Ram
+	fmt.Println("str:", str)
+	slice := strings.Split(str, " ")
+	fmt.Println("slice:", slice)
+	var str1 string
+	for _, ele := range slice {
+		bs := []byte(ele)
+		bs[0] = byte(bs[0] - 32)
+		str1 = str1 + string(bs) + " "
+	}
+	fmt.Println("str1:", str1)
+
 }
