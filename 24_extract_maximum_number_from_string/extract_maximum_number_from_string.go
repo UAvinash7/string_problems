69d9a1db179c21da005459e6957bfac826c559f3
avinash upadhyayula <avinash.upadhyayula@gmail.com>
2025-08-03 22:25:33 +0530

    continuation

--- 24_extract_maximum_number_from_string/extract_maximum_number_from_string.go
+++ 24_extract_maximum_number_from_string/extract_maximum_number_from_string.go
@@ -10,6 +10,27 @@ Extract maximum number from string
 
 package main
 
+import (
+	"fmt"
+	"strings"
+)
+
 func main() {
-	
+	str := "100ab546sc200" // output = 546
+	var result string
+	for _, ele := range str {
+		if string(ele) < "a" || string(ele) > "z" {
+			result += string(ele)
+		} else {
+			result += " "
+		}
+	}
+	arr := strings.Split(result, " ")
+	num := "0"
+	for i := 0; i < len(arr); i++ {
+		if num < arr[i] {
+			num = arr[i]
+		}
+	}
+	fmt.Println("num:", num)
 }
