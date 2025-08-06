c7c00e424e9ac514b99c567a6e0459a832bfbd4e
avinash upadhyayula <avinash.upadhyayula@gmail.com>
2025-08-06 23:11:23 +0530

    continuation

--- 21_anagram_check/program_to_check_anagram.go
+++ 21_anagram_check/program_to_check_anagram.go
@@ -10,6 +10,27 @@ Program to check anagram
 
 package main
 
-func main() {
+import "fmt"
 
+func main() {
+	str1 := "anagram"
+	str2 := "nagaram"
+	mpp := make(map[string]int)
+	for i := 0; i < len(str1); i++ {
+		mpp[string(str1[i])]++
+	}
+	for i := 0; i < len(str2); i++ {
+		mpp[string(str2[i])]--
+	}
+	result := 1
+	for i := 0; i < len(str1); i++ {
+		if mpp[string(str1[i])] != 0 {
+			result = 0
+		}
+	}
+	if result == 1 {
+		fmt.Println("Yes, this is an anagram")
+	} else {
+		fmt.Println("No, this is not an anagram")
+	}
 }
