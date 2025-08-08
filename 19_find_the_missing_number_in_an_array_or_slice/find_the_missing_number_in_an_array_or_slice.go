c7561733a992c8a98567f896abc416e24ccb9a9e
avinash upadhyayula <avinash.upadhyayula@gmail.com>
2025-08-08 22:38:13 +0530

    continuation

--- 19_find_the_missing_number_in_an_array_or_slice/find_the_missing_number_in_an_array_or_slice.go
+++ 19_find_the_missing_number_in_an_array_or_slice/find_the_missing_number_in_an_array_or_slice.go
@@ -10,6 +10,14 @@ Find the missing number in an array or slice.
 
 package main
 
+import "fmt"
+
 func main() {
-	
+	slice := []int{2, 4, 1, 5, 7, 8}
+	num := len(slice)
+	total := (num + 1) * (num + 2) / 2
+	for i := 0; i < num; i++ {
+		total = total - slice[i]
+	}
+	fmt.Println("missing number:", total)
 }
