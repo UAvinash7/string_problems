0ead277598cef2c23475330f2fffdfab468c00ce
avinash upadhyayula <avinash.upadhyayula@gmail.com>
2025-08-09 22:35:45 +0530

    continuation

--- 18_lower_to_higher_numbers_using_a_slice/lower_to_higher_and_higher_to_lower_number_using_a_slice.go
+++ 18_lower_to_higher_numbers_using_a_slice/lower_to_higher_and_higher_to_lower_number_using_a_slice.go
@@ -10,6 +10,18 @@ Lower to higher numbers and higher to lower numbers using a slice.
 
 package main
 
+import "fmt"
+
 func main() {
-	
+	slice := []int{2, 1, 3, 5, 4, 7, 6}
+	for i := 0; i < len(slice); i++ {
+		for k := 0; k < len(slice)-1; k++ {
+			if slice[k] > slice[k+1] {
+				temp := slice[k+1]
+				slice[k+1] = slice[k]
+				slice[k] = temp
+			}
+		}
+	}
+	fmt.Println("slice:", slice)
 }
