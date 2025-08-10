5be3136e207c91027a5d0c27a3af0b4cf7ff6d0e
avinash upadhyayula <avinash.upadhyayula@gmail.com>
2025-08-10 22:29:08 +0530

    continuation

--- 20_palindrome_check/program_to_check_palindrome.go
+++ 20_palindrome_check/program_to_check_palindrome.go
@@ -10,6 +10,17 @@ Program to check palindrome
 
 package main
 
+import "fmt"
+
 func main() {
-	
+	str := "ABBA"
+	result := []byte{}
+	for i := len(str) - 1; i >= 0; i-- {
+		result = append(result, str[i])
+	}
+	if str == string(result) {
+		fmt.Println("This is palindrome")
+	} else {
+		fmt.Println("This is not palindrome")
+	}
 }
