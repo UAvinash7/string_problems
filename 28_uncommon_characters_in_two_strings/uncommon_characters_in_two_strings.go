9d2a428d5a975d259ecd82ccc54a61516f5abdf5
avinash upadhyayula <avinash.upadhyayula@gmail.com>
2025-08-16 21:30:36 +0530

    continuation

--- 28_uncommon_characters_in_two_strings/uncommon_characters_in_two_strings.go
+++ 28_uncommon_characters_in_two_strings/uncommon_characters_in_two_strings.go
@@ -10,6 +10,23 @@ Uncommon characters in two strings
 
 package main
 
+import "fmt"
+
 func main() {
-	
+	str1 := "afbde"
+	str2 := "wabq"
+	mpp := make(map[string]int)
+	for i := 0; i < len(str1); i++ {
+		mpp[string(str1[i])]++
+	}
+	for i := 0; i < len(str2); i++ {
+		mpp[string(str2[i])]++
+	}
+	var result string
+	for index, element := range mpp {
+		if element <= 1 {
+			result = result + index
+		}
+	}
+	fmt.Println("result:", result)
 }
